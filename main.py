9e1218dc78bd9dc295b20fba330957ffc260b08b
Andon Li <andonli281@gmail.com>
2025-12-20 14:36:30 -0500

    update requirements, estabished format for answers.yaml, init main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import yaml
+
+
+# Load answers.json into memory
+with open('answers.yml', 'r') as file:
+    print(yaml.safe_load(file))
 
 
 
-
-if __name__ == '__main__':
-    app.run(port=4000)
+# if __name__ == '__main__':
+#     app.run(port=4000)
